a296d8bbba8783d60628871d8fa1838155325504
fredrick osuala <fredosuala@gmail.com>
2020-01-09 13:44:06 +0100

    Updates

---
+++ precache-manifest.bcc0b29ffa05092220c25cdc7e260951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b97c10bf7fccb7e20fc7da3da8b9932",
+    "url": "/uzo3fo/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05bee399c3f1bd3d6510",
+    "url": "/uzo3fo/monsters-rolodex/static/css/main.923e6bcb.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/uzo3fo/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/uzo3fo/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05bee399c3f1bd3d6510",
+    "url": "/uzo3fo/monsters-rolodex/static/js/main.181814d1.chunk.js"
+  },
+  {
+    "revision": "e0daa2971796cbc62b33",
+    "url": "/uzo3fo/monsters-rolodex/static/js/runtime-main.d96fcaf9.js"
+  }
+]);
